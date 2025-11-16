986e037108fa488cad0cafdc0d79858e35940b11
Xiaoph1 <3500423502@qq.com>
2025-11-16 16:27:28 +0800

    Display Data in recycleView

--- app/src/main/java/com/example/todoapp/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/ListFragment.kt
@@ -7,11 +7,19 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapp.data.viewmodel.ToDoViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-
 class ListFragment : Fragment() {
+
+    private val mToDoViewModel:ToDoViewModel by viewModels()
+
+    private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +27,14 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer{ data->
+            adapter.setData(data)
+        })
 
         val fab = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
         fab.setOnClickListener {
